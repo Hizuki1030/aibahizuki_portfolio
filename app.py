264d17ad185aefbba38999524d0b64f2109f922b
1030 <45566778+Hizuki1030@users.noreply.github.com>
2021-09-14 11:17:00 +0900

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,url_for,send_from_directory #追加
 import os
 app = Flask(__name__)
 
+
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
@@ -15,18 +16,19 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico')
 
 @app.route('/')
 def main():
     name = "Hoge"
     return render_template('index.html', title='flask test', name=name) #変更
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 ## おまじない
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
