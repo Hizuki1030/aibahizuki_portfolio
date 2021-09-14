d13ed1017715e99fd0fc55bdaef6eeb0c98a0f5c
1030 <45566778+Hizuki1030@users.noreply.github.com>
2021-09-14 10:33:28 +0900

    update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
+    return app.send_static_file('favicon.ico')
 
 ## おまじない
 if __name__ == "__main__":
