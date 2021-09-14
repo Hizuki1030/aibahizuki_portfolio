e664f2d33d0c3a80a18c1e847b8b6aab361f4ee9
1030 <45566778+Hizuki1030@users.noreply.github.com>
2021-09-14 10:29:01 +0900

    update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
 ## おまじない
 if __name__ == "__main__":
